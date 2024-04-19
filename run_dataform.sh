0ccab6731e983965336a22c73b4831e13f177403
ddeleo <danieldeleo@google.com>
2024-04-19 17:35:12 -0400

    fix

--- run_dataform.sh
+++ run_dataform.sh
@@ -25,7 +25,15 @@ fi
 printf """
 Executing the following dataform command:
 ************************************************************
-dataform run --run-tests --vars=runUnitTests=true %s %s
+dataform test --vars=runUnitTests=true --run-tests %s %s 
 ************************************************************
 """ "${all_dataform_tags}" "${all_dataform_actions}"
-dataform run --run-tests --vars=runUnitTests=true $(echo "${all_dataform_tags}" "${all_dataform_actions}" | xargs)
+dataform test $(echo "${all_dataform_tags}" "${all_dataform_actions}" | xargs)
+
+printf """
+Executing the following dataform command:
+************************************************************
+dataform run %s %s
+************************************************************
+""" "${all_dataform_tags}" "${all_dataform_actions}"
+dataform run $(echo "${all_dataform_tags}" "${all_dataform_actions}" | xargs)
