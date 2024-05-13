e7617c440b49677978811aba27158a2dcc2aef78
Vince Gonzalez <vincegonzalez@google.com>
2024-05-13 15:51:29 -0400

    adding changes to backend, datasets

--- run_terraform.sh
+++ run_terraform.sh
@@ -1,9 +1,25 @@
-cd terraform
+#!/bin/sh -ex
 
-if [ -d "environments/$BRANCH_NAME/" ]; then
-    cd environments/$BRANCH_NAME      
-    terraform init
-    terraform apply -auto-approve
+if [ -z "${BRANCH_NAME}" ]; then
+  BRANCH_NAME=$(git branch --show-current)
+fi
+echo "Using '${BRANCH_NAME}' environment"
+
+# Edit these variables.
+TF_BACKEND_BUCKET="zr-dev-vincegonzalez"
+TF_BACKEND_PREFIX="terraform/state/${BRANCH_NAME}"
+
+cd terraform || (echo "Could not change directory."; exit)
+
+DIR="environments/${BRANCH_NAME}/"
+if [ -d "${DIR}" ]; then
+    cd "${DIR}" || (echo "Could not cd to ${DIR}"; exit)
+    terraform init \
+        -backend-config="bucket=${TF_BACKEND_BUCKET}" \
+        -backend-config="prefix=${TF_BACKEND_PREFIX}"
+    terraform apply -auto-approve \
+        -var-file="../../../${BRANCH_NAME}.tfvars"
+
 else
     echo "***************************** SKIPPING APPLYING *******************************"
     echo "Branch '$BRANCH_NAME' does not represent an official environment."
--- terraform/environments/prod/datasets.tf
+++ terraform/environments/prod/datasets.tf
@@ -1,12 +1,12 @@
 module "bigquery-dataset" {
   source     = "github.com/GoogleCloudPlatform/cloud-foundation-fabric//modules/bigquery-dataset?ref=v30.0.0&depth=1"
-  project_id = "danny-bq"
-  id         = "example_dataset"
-  location   = "US"
+  project_id = var.project_id
+  id         = var.dataset_id
+  location   = var.location
   access = {
     owner          = { role = "OWNER", type = "user" }
   }
   access_identities = {
-    owner          = "ddeleo@google.com"
+    owner          = var.owner
   }
 }
